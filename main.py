2a2b169646b074528a00e43636e583a39e8f0ca8
sailorsinbad <sailorsinbad14@gmail.com>
2019-04-27 12:56:48 -0700

    Add main func

---
+++ main.py
@@ -0,0 +1,5 @@
+from useSpeechToText import useSpeechToText
+
+ffmpegAddress = r"C:\Program Files\ffmpeg\bin\ffmpeg.exe"
+path = r'C:\Users\lbm4\PycharmProjects\Vocaloid\resources\japan.mp3'
+useSpeechToText(ffmpegAddress, path)

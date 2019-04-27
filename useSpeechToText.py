da1670edc298137fdc9f9e98ce762ced997e2660
sailorsinbad <sailorsinbad14@gmail.com>
2019-04-27 05:40:54 -0700

    2019 Citrus Hack Vocaloid Project Complete the function and can detect which language it is now

--- useSpeechToText.py
+++ useSpeechToText.py
@@ -3,7 +3,8 @@ import os
 
 
 # Imports the Google Cloud client library
-from google.cloud import speech
+from google.cloud import speech_v1p1beta1 as speech
+# from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
@@ -16,12 +17,14 @@ client = speech.SpeechClient()
 from pydub import AudioSegment
 AudioSegment.converter = r"C:\Program Files\ffmpeg\bin\ffmpeg.exe"
 # AudioSegment.converter = r"C:\Program Files (x86)\avconv\usr\bin\avconv.exe"
-sound = AudioSegment.from_wav(r"C:\Users\lbm4\PycharmProjects\Vocaloid\resources\commercial_mono.wav")
+# sound = AudioSegment.from_wav(r"C:\Users\lbm4\PycharmProjects\Vocaloid\resources\commercial_mono.wav")
 
-sound2 = AudioSegment.from_mp3(r"C:\Users\lbm4\PycharmProjects\Vocaloid\resources\hibiki.mp3")
-print (sound.frame_rate)
-print(sound.channels)
-sound.export(r"C:\Users\lbm4\PycharmProjects\Vocaloid\resources\commercial_mono.flac", format="flac")
+# sound2 = AudioSegment.from_mp3(r"C:\Users\lbm4\PycharmProjects\Vocaloid\resources\twinkles.mp3")
+
+sound3 = AudioSegment.from_mp3(r"C:\Users\lbm4\PycharmProjects\Vocaloid\resources\japan.mp3")
+# print (sound3.frame_rate)
+# print(sound3.channels)
+sound3.export(r"C:\Users\lbm4\PycharmProjects\Vocaloid\resources\japan.flac", format="flac")
 # from  scipy.io import wavfile
 # rate, data = wavfile.read(r"C:\Users\lbm4\PycharmProjects\Vocaloid\resources\feifan.wav")
 # data0 = data [: , 0].tobytes()
@@ -29,19 +32,23 @@ sound.export(r"C:\Users\lbm4\PycharmProjects\Vocaloid\resources\commercial_mono.
 file_name = os.path.join(
     os.path.dirname(__file__),
     'resources',
-    'commercial_mono.flac')
-
+    'japan.flac')
+first_lang = 'en-US'
+second_lang = 'ja-JP'
+third_lang = 'zh'
 # Loads the audio into memory
 with io.open(file_name, 'rb') as audio_file:
     content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
+    audio = speech.types.RecognitionAudio(content=content)
 
-config = types.RecognitionConfig(
+config = speech.types.RecognitionConfig(
     encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-    sample_rate_hertz=8000,
-    audio_channel_count=1,
-    language_code='en-US',
+    sample_rate_hertz=sound3.frame_rate,
+    audio_channel_count=sound3.channels,
+    language_code=first_lang,
+    alternative_language_codes=[second_lang, third_lang],
     enable_word_time_offsets=True
+
 )
 
 
@@ -52,8 +59,10 @@ operation = client.long_running_recognize(config, audio)
 print (file_name,)
 response = client.recognize(config, audio)
 #print(response)
+
 for result in response.results:
     alternative = result.alternatives[0]
+
     print('Transcript: {}'.format(result.alternatives [0].transcript))
 
     for word_info in alternative.words:
@@ -64,3 +73,16 @@ for result in response.results:
             word,
             start_time.seconds + start_time.nanos * 1e-9,
                 end_time.seconds + end_time.nanos * 1e-9))
+
+from langdetect import detect
+lang = detect(result.alternatives [0].transcript)
+if(lang == 'ja'):
+    print("This is Japanese!")
+elif(lang =='zh'):
+    print("This is Simplified Chinese!")
+elif(lang == 'en-US'):
+    print("This is English!")
+else:
+    print("Cannot recognize this language...")
+
+

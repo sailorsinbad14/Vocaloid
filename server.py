273e6d80558a587d92abed88cb18732612d7f299
Kevin <kevinfang28@gmail.com>
2019-04-28 05:00:42 -0700

    website

--- server.py
+++ server.py
@@ -1,15 +1,25 @@
 from flask import Flask, jsonify
 from useSpeechToText import useSpeechToText
 
-app = Flask(__name__)
-ffmpegAddress = r"C:\Program Files\ffmpeg\bin\ffmpeg.exe"
-path = r'resources/twinkles.mp3'
+app = Flask(__name__, static_folder='website')
 
 @app.route("/")
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
+
+@app.route("/test")
 def testing():
-    same, display, language = useSpeechToText(ffmpegAddress,path)
+    app.logger.debug(request.files['file'].filename)
+    timestamps, display, language = useSpeechToText()
+
+    timestamps
+
     return_dict = {}
-    return_dict["timestamps"] = same
+    return_dict["timestamps"] = timestamps
     return_dict["display"] = display
     return_dict["language"] = language
 
